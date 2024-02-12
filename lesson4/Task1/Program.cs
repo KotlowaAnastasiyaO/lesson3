fee9378460690e0963e857ab04de6847b021c91b
KotlowaAO <ya@kotlowaa.ru>
2024-02-12 18:17:03 +0300

    add lesson4:Task1

---
+++ lesson4/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая принимает на вход трехзначное число и удалят цифру этого числа.
+Примеры
+a =256 => 26
+a = 891 => 81
+*/
+
+int num = 123;
+int firstNum = num / 100;
+int thirdNum = num % 10;
+int resultNum = firstNum * 10 + thirdNum;
+Console.WriteLine(resultNum);
