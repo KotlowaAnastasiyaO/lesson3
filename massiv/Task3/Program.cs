77101d31a4468e3780fd202f364437fa44f59b3e
KotlowaAO <ya@kotlowaa.ru>
2024-02-18 16:22:51 +0300

    add Task3:Massive

---
+++ massiv/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//произведение пар чисел в одномерном массиве
+//парой считаем первый и послепдний элемент массива
+//результат записать в новый массив
+
+int [] array = new int [] {1, 2, 3, 4, 5, 7, 8};
+int [] printArr = new int [array.Length/2];
+
+for(int i = 0; i<array.Length/2;i++){
+    printArr[i] = array[i] * array[array.Lenght-1-i];
+}
+for(int i = 0; i<printArr.Lenght; i++){
+    Console.Write($"{printArr[i]} ");
+}
+
