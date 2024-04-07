86a4c068e0c371891c6236baabd5f0d9f36cb58f
KotlowaAO <ya@kotlowaa.ru>
2024-04-07 17:05:42 +0300

    задачка с функцией вычисления производного двух вычислений

---
+++ function/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+	double numinatre = a * b;
+	int denuminate = c + d;
+	double result = numinatre / denuminate;
+	return result;
+}
+
+double result = CalculateFormula(1,2,3,4);
+Console.WriteLine(result);
