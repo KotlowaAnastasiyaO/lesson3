eb82b347092c1794783d4d3949f4b664eaa3f388
KotlowaAO <ya@kotlowaa.ru>
2024-04-07 18:36:27 +0300

    ввод данных рандомный смассива

---
+++ function/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Random rnd = new Random();
+
+int size = 30;
+int [] arr1 = new int [size];
+int i = 0;
+while(i < size)
+{
+	arr1 [i] = rnd.Next(1,10);
+	i++;
+}
+
+i = 0;
+while(i < size)
+{
+	Console.Write($"{arr1[i]} ");
+	i++;
+}
