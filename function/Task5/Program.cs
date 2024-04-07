5e7355cee88df2a60b680047da7dd72672ce1496
KotlowaAO <ya@kotlowaa.ru>
2024-04-07 18:52:36 +0300

    ввод данных с консоли

---
+++ function/Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿int size = 10;
+int [] arr1 = new int [size];
+int i = 0;
+while(i < size)
+{
+	Console.WriteLine("Введите целое число:");
+    string input = Console.ReadLine();
+	arr1 [i] = Convert.ToInt32(input);
+	i++;
+}
+
+i = 0;
+while(i< size)
+{
+	Console.Write($"{arr1[i]} ");
+	i++;
+}
